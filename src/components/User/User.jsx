c4d1b7f0092399cfe392cbf7383666139fb2136d
tanvirtbf <tanver018765@gmail.com>
2024-04-03 11:14:33 +0600

    creating user ui and get parameter using useParams

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-
+import { useParams } from "react-router-dom";
 function User() {
-    const {userid} = useParams()
+  const { userid } = useParams();
   return (
-    <div className='bg-gray-600 text-white text-3xl p-4'>User: {userid}</div>
-  )
+    <div className="bg-gray-600 text-white text-3xl p-4">User:{userid}</div>
+  );
 }
 
-export default User
+export default User;
