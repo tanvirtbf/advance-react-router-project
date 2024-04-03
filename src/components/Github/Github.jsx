405e9e3670638451528866ee3f1b8b7d0b49fe10
tanvirtbf <tanver018765@gmail.com>
2024-04-03 11:49:53 +0600

    Complete Project

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -24,6 +24,6 @@ function Github() {
 export default Github;
 
 export const githubInfoLoader = async () => {
-  const response = await fetch("https://api.github.com/users/hiteshchoudhary");
+  const response = await fetch("https://api.github.com/users/tanvirtbf");
   return response.json();
 };
