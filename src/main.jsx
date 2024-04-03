ff8fae2a2ec688a9a9cc4940ea00c8f4b0d321cd
tanvirtbf <tanver018765@gmail.com>
2024-04-03 10:30:14 +0600

    setup router element and outlet

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './Layout'
+import Home from './components/Home/Home'
+import About from './components/About/About'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
-    children: []
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />
+      }
+    ]
   }
 ])
 
